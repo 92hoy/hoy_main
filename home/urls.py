3292ff30cbb66e80b99e159bf9e6d22f1ef67d1f
92hoy <ho_9209@naver.com>
2019-07-18 17:39:58 +0900

    DB.user table / signin ADD

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main, name='main'),
+    url(r'^sign/$', views.sign, name='sign'),
     # url(r'^course_manage/list/$', views.course_manage_list, name='course_manage_list'),
     # url(r'^progress/list/(?P<course_seq>.*?)$', views.progress_list, name='progress_list'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 
 # Create your views here.
+def sign(request):
+    return render(request, 'sign/sign.html')
+
 
 def main(request):
     return render(request, 'main/main.html')
