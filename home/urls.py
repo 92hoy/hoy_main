b4fd7c2c7e7f1fdc15b901ae4fbce77b9bc4444a
92hoy <ho_9209@naver.com>
2019-07-18 11:34:40 +0900

    first commit

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^/$', views.course_manage_list, name='course_manage_list'),
+    # url(r'^course_manage/list/$', views.course_manage_list, name='course_manage_list'),
+    # url(r'^progress/list/(?P<course_seq>.*?)$', views.progress_list, name='progress_list'),
+]
